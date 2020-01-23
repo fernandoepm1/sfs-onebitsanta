b55226f344312460ae35fc6fb876095def794acf
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-01-22 21:07:41 -0300

    Configure and mount sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  # Not safe to define an open route like this
+  mount Sidekiq::Web => '/sidekiq'
 end
