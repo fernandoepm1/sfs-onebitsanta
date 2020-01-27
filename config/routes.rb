57a38b4eb41eb950bbd3f83b426b88f2f74b4d17
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-01-27 18:26:32 -0300

    Generate Pages controller and assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   # Not safe to define an open route like this
