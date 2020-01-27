906d981494878f57b10529b7115a8ba9a5dd5ac1
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-01-27 18:58:25 -0300

    Update gatherings table with event_date and event_hour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_233225) do
+ActiveRecord::Schema.define(version: 2020_01_27_215617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_01_24_233225) do
     t.text "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "event_date"
+    t.string "event_hour"
     t.index ["user_id"], name: "index_gatherings_on_user_id"
   end
 
