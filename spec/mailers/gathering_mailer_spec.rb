a953ba0fd67b3a83722e77ed726f8ad00185415c
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-02-06 18:28:44 -0300

    Create initial mailer test to fail

--- spec/mailers/gathering_mailer_spec.rb
+++ spec/mailers/gathering_mailer_spec.rb
@@ -2,17 +2,29 @@ require "rails_helper"
 
 RSpec.describe GatheringMailer, type: :mailer do
   describe "raffle" do
-    let(:mail) { GatheringMailer.raffle }
+    before do
+      @gathering = create(:gathering)
+      @member    = create(:member, gathering: @gathering)
+      @friend    = create(:member, gathering: @gathering)
+      @mailer    = GatheringMailer.raffle(@gathering, @member, @friend)
+    end
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Raffle")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(@mailer.subject).to eq("Secret Santa: #{@gathering.title}")
+      expect(@mailer.to).to eq([@member.email])
+      #expect(@mailer.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the member name in the body" do
+      expect(@mailer.body.encoded).to match(@member.name)
+    end
+
+    it "renders the gathering organizer name in the body" do
+      expect(@mailer.body.encoded).to match(@gathering.user.name)
+    end
+
+    it "renders the member link to open in the body" do
+      expect(@mailer.body.encoded).to match("/members/#{@member.token}/opened")
     end
   end
-
 end
