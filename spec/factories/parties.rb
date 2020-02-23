df130b6a0dbb09ef18b4bd62836f60e1af09446d
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-02-22 22:03:35 -0300

    Update party model and factory

--- spec/factories/parties.rb
+++ spec/factories/parties.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :party do
+    user
     title       { FFaker::Lorem.word }
     description { FFaker::Lorem.sentence }
-    user
+    location { "#{FFaker::Address.city}, #{FFaker::Address.street_address}" }
+    event_date { FFaker::Time.date }
+    event_hour { rand(24).to_s }
   end
 end
